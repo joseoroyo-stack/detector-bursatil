e65bdb8bd4593a7e9624a182209de17eceebeda4
TU NOMBRE <TU-EMAIL@ejemplo.com>
2025-09-23 02:44:01 +0200

    Versión nueva de Tradepulse

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 // app/page.tsx
 import { redirect } from "next/navigation";
-
-// opcional, pero ayuda a que no lo prerendere
 export const dynamic = "force-dynamic";
 
 export default function Home() {
-  // Redirige SIEMPRE el home a la landing
   redirect("/landing");
 }
