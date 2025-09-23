e726b6bdeb7bde5c3838286d3707c4b33d75e850
TU NOMBRE <TU-EMAIL@ejemplo.com>
2025-09-23 20:39:31 +0200

    Update page.tsx

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -3,4 +3,4 @@ export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 // Reexporta la plataforma desde app/home-client.tsx (singular)
-export { default } from "@/app/home-client";
+export { default } from "../home-client";
