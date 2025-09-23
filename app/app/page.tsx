550ddb3e6741fc58108753d5199ad4e48aced0cb
TU NOMBRE <TU-EMAIL@ejemplo.com>
2025-09-23 19:45:49 +0200

    Versión nueva de Tradepulse

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -3,4 +3,4 @@ export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 // Reexporta la plataforma desde app/home-clients.tsx
-export { default } from "@/app/home-clients";
+export { default } from "../home-clients";
